e971220aab478c9795c54a7ee5f812e89699ece5
calebeLAR <ca6ele@gmail.com>
2023-12-23 16:25:49 -0300

    trybe.fundamentos.seção03.dia04.pagina06c.js

---
+++ section03/dia04_objetos/pagina06/pagina06c.js
@@ -0,0 +1,7 @@
+class Pagina06 {
+    #Exercícios_Objetos;
+    #Manipulação_de_objetos_Parte01;
+    #Manipulação_de_objetos_Parte02;
+    #Manipulação_de_objetos_Parte03;
+    #Manipulação_de_objetos_Parte04;
+}
