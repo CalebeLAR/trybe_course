35e5d6496fd9998b780a04c901e776d796e09383
calebeLAR <ca6ele@gmail.com>
2023-12-27 10:25:55 -0300

    fundamentals.section03.day06.project

---
+++ section03/dia06_projeto_supermecado_pirilampo/pagina01/pagina01c.js
@@ -0,0 +1,10 @@
+class Pagina01 {
+    #Por_que_isso_é_importante;
+    #Especificação;
+    #O_que_vamos_avaliar;
+    #O_que_devo_desenvolver;
+    #Entregáveis;
+    #Tenha_atenção_e_siga_as_instruções_do_arquivo_READMEmd_do_repositório;
+}
+
+
