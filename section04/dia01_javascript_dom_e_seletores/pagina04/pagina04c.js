64a10a77353a021bcd6e1f3cee0d2af2a71f15f8
calebeLAR <ca6ele@gmail.com>
2024-01-01 14:23:30 -0300

    trybe.fundamentos.seção04.dia01.pagina04c.js

---
+++ section04/dia01_javascript_dom_e_seletores/pagina04/pagina04c.js
@@ -0,0 +1,8 @@
+class Pagina04 {
+    #Recuperando_elementos_html_funções_query;
+    #queryselector_recuperando_um_elemento_html;
+    #queryselectorall_recuperando_elementos_html;
+    #qual_seletor_css_utilizar_nas_funcoes_query;
+    #diferenca_entre_as_funcoes_get_e_query_que_retornam_mais_de_um_elemento;
+    #para_fixar;
+}
