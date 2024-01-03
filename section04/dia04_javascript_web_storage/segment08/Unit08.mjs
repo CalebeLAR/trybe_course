3b64118a150a12e7e9b6b12d83b54259c383d04a
calebeLAR <ca6ele@gmail.com>
2024-01-03 15:48:01 -0300

    trybe.fundamentos.seção03.dia04.Unit08c.mjs

---
+++ section04/dia04_javascript_web_storage/segment08/Unit08.mjs
@@ -0,0 +1,7 @@
+export default class Unit08 {
+    title = 'Fixando os aprendizados';
+    topics = [
+        'Fixando os aprendizados',
+        'Fixando os aprendizados - Resultado 90%',
+    ];
+}
