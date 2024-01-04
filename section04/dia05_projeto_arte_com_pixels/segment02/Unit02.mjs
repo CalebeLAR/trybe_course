7252fb4611795727c32a9e62b8edfb4218c6455f
calebeLAR <ca6ele@gmail.com>
2024-01-04 15:36:12 -0300

    trybe.fundamentos.seção04.dia05.Unit02c.mjs

---
+++ section04/dia05_projeto_arte_com_pixels/segment02/Unit02.mjs
@@ -0,0 +1,14 @@
+export default class Unit02 {
+    static title = 'Projeto Arte com Pixels';
+    static topics = [
+        'Projeto Pixels Art',
+        'O que vamos fazer?',
+        'Por que isso é importante?',
+        'Especificação',
+        'O que vamos avaliar?',
+        'Entregáveis',
+        'Fique atento e siga as instruções no README.md do repositório! 🥺',
+        'Recursos adicionais (opcional)',
+        'Avalie o conteúdo',
+    ];
+}
