41cfb7719abb063570ed11804cfce63757a9ddde
calebeLAR <ca6ele@gmail.com>
2024-01-04 15:36:12 -0300

    trybe.fundamentos.seção04.dia05.Unit01c.mjs

---
+++ section04/dia05_projeto_arte_com_pixels/segment01/Unit01.mjs
@@ -0,0 +1,9 @@
+export default class Unit03 {
+    static title = 'Introdução - JavaScript - Projetos';
+    static topics = [
+        'Introdução - JavaScript - Projetos',
+        'O que vamos fazer?',
+        'Coloque seus projetos no ar!',
+        'Bônus',
+    ];
+}
