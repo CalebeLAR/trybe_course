d15378fce46ba09eb21fc5e17d02e598a5ba834b
calebeLAR <ca6ele@gmail.com>
2024-01-05 14:30:00 -0300

    trybe.fundamentos.seção04.dia06.Unit02c.mjs

---
+++ section04/dia06_projetos_bonus/segment02/Unit02.mjs
@@ -0,0 +1,15 @@
+export default class Unit02 {
+    static title = '(Bônus) Projeto Meme Generator';
+    static topics = [
+        '(Bônus) Projeto Meme Generator',
+        'O que vamos fazer?',
+        'Por que isso é importante?',
+        'Especificação',
+        'O que vamos avaliar?',
+        'O que devo desenvolver?',
+        'Dicas',
+        'Entregáveis',
+        'Fique atento e siga as instruções no README.md do repositório! 🥺',
+        'Recursos adicionais (opcional)',
+    ];
+}
