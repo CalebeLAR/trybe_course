5489a1882c99e68ee3861db95e3baaa66000140f
calebeLAR <ca6ele@gmail.com>
2024-01-08 15:50:36 -0300

    trybe.fundamentos.seção05.dia01.Unit03c.mjs

---
+++ section05/dia01_html_e_css_forms/segment03/Unit03.mjs
@@ -0,0 +1,8 @@
+export default class Unit03 {
+  static title = 'input tag, types e attributes';
+  static topics = [
+    'Input tag, types e attributes',
+    'Para fixar',
+    'Exercício 1',
+  ];
+}
