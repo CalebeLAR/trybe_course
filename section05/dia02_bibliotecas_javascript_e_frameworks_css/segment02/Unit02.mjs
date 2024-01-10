6fa5c368c18259b47a2218f10f2b5a4fc2927179
calebeLAR <ca6ele@gmail.com>
2024-01-10 15:25:18 -0300

    trybe.fundamentos.seção05.dia02.Unit02c.mjs

---
+++ section05/dia02_bibliotecas_javascript_e_frameworks_css/segment02/Unit02.mjs
@@ -0,0 +1,8 @@
+export default class Unit02 {
+  static title = 'Bibliotecas JavaScript';
+  static topics = [
+    'Bibliotecas JavaScript',
+    'A biblioteca trará uma boa experiência para quem a utiliza?',
+    'A biblioteca trará uma boa experiência para a pessoa usuária?',
+  ];
+}
