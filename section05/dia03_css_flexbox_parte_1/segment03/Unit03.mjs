fb015d938708fbee28a892f58de7ce0433f5a4de
calebeLAR <ca6ele@gmail.com>
2024-01-11 18:59:25 -0300

    trybe.fundamentos.seção05.dia03.Unit03c.mjs

---
+++ section05/dia03_css_flexbox_parte_1/segment03/Unit03.mjs
@@ -0,0 +1,10 @@
+export default class Unit03 {
+  static title = 'Introdução - flex-direction, flex-wrap, flex-flow e reverse';
+  static topics = [
+    'Introdução, estrutura básica, flex-direction, flex-wrap, flex-flow e reverse',
+    'Para fixar',
+    'Exercício 1',
+    'Exercício 2',
+    'Exercício 3',
+  ];
+}
