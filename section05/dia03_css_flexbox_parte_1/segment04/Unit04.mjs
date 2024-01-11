2ea5aa9709dfc1e557667cd1b6e81161195261f0
calebeLAR <ca6ele@gmail.com>
2024-01-11 18:59:25 -0300

    trybe.fundamentos.seção05.dia03.Unit04c.mjs

---
+++ section05/dia03_css_flexbox_parte_1/segment04/Unit04.mjs
@@ -0,0 +1,10 @@
+export default class Unit04 {
+  static title = 'Propriedades: justify-content, align-items e align-content';
+  static topics = [
+    'Propriedades: justify-content, align-items e align-content',
+    'Para fixar',
+    'Exercício 1',
+    'Exercício 2',
+    'Exercício 3',
+  ];
+}
