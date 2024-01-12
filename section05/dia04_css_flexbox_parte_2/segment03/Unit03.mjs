9af324837c9d484b6e33098d37ba38afc96b48c9
calebeLAR <ca6ele@gmail.com>
2024-01-12 18:45:22 -0300

    trybe.fundamentos.seção05.dia04.Unit03c.mjs

---
+++ section05/dia04_css_flexbox_parte_2/segment03/Unit03.mjs
@@ -0,0 +1,10 @@
+export default class Unit03 {
+  static title = 'Propriedades: flex-grow, flex-shrink, flex-basis';
+  static topics = [
+    'Propriedades: flex-grow, flex-shrink, flex-basis',
+    'Para fixar',
+    'Exercício 1',
+    'Exercício 2',
+    'Exercício 3',
+  ];
+}
