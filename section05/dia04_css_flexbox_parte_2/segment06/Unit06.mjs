f2627e1d2b1769672decb271d00797283f8137f5
calebeLAR <ca6ele@gmail.com>
2024-01-12 18:45:22 -0300

    trybe.fundamentos.seção05.dia04.Unit06c.mjs

---
+++ section05/dia04_css_flexbox_parte_2/segment06/Unit06.mjs
@@ -0,0 +1,16 @@
+export default class Unit06 {
+  static title = 'Vamos praticar';
+  static topics = [
+    'Vamos praticar!',
+    'Exercício 1',
+    'Exercício 2',
+    'Exercício 3',
+    'Exercício 4',
+    'Exercício 5',
+    'Exercício 6',
+    'Exercício 7',
+    'Exercício 8',
+    'Exercício 9',
+    'Extra (opcional)',
+  ];
+}
