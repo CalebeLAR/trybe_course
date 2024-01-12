52af137133f01b32708eccc3b4cf5a9386254251
calebeLAR <ca6ele@gmail.com>
2024-01-12 18:45:22 -0300

    trybe.fundamentos.seção05.dia04.Unit02c.mjs

---
+++ section05/dia04_css_flexbox_parte_2/segment02/Unit02.mjs
@@ -0,0 +1,11 @@
+export default class Unit02 {
+  static title = 'Flex Items';
+  static topics = [
+    'Flex items',
+    'Propriedade align-self',
+    'Propriedade order',
+    'Para fixar',
+    'Exercício 1',
+    'Exercício 2',
+  ];
+}
