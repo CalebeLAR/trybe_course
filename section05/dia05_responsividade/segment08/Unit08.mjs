24c93caa49a671cfa5ebff77aeb25d2709dc341e
calebeLAR <ca6ele@gmail.com>
2024-01-15 15:15:59 -0300

    trybe.fundamentos.seção05.dia05.Unit08c.mjs

---
+++ section05/dia05_responsividade/segment08/Unit08.mjs
@@ -0,0 +1,9 @@
+export default class Unit08 {
+  static title = 'Execícios - Criar página para tela pequena';
+  static topics = [
+    'Exercício 3 - 🚀 Criar uma página para uma tela pequena',
+    'Exercício 3.1 - Execute as seguinte estilizações para todo o corpo da página',
+    'Exercício 3.2 - Defina as propriedades css para os elementos',
+    'Exercício 3.3 - Construindo uma página responsiva',
+  ];
+}
