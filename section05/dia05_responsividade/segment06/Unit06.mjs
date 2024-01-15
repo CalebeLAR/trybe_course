3f2a6f144fd96e0f2b1f992ac523561c8fafbd01
calebeLAR <ca6ele@gmail.com>
2024-01-15 15:15:59 -0300

    trybe.fundamentos.seção05.dia05.Unit06c.mjs

---
+++ section05/dia05_responsividade/segment06/Unit06.mjs
@@ -0,0 +1,10 @@
+export default class Unit06 {
+  static title =
+    'Exercícios - Criando layouts para dispositivos móveis e para impressão';
+  static topics = [
+    'Vamos praticar!',
+    'Exercício 1 Criando layouts para dispositivos móveis e impressão',
+    'Exercício 1.1 Crie uma versão de layout para impressão',
+    'Exercício 1.2 Crie uma versão de layout para telas pequenas',
+  ];
+}
