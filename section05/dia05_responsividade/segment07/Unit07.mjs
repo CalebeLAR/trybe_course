d909746df78a98a4e5bb1a094dff6ca02e334da2
calebeLAR <ca6ele@gmail.com>
2024-01-15 15:15:59 -0300

    trybe.fundamentos.seção05.dia05.Unit07c.mjs

---
+++ section05/dia05_responsividade/segment07/Unit07.mjs
@@ -0,0 +1,8 @@
+export default class Unit07 {
+  static title =
+    'Exercícios - Criando 3 layouts diferentes utilizando o mobile first';
+  static topics = [
+    'Exercício 2 🚀 Criando 3 layouts diferentes utilizando o mobile first',
+    'Exercício 2.1 Crie três versões de layout diferentes para essa página',
+  ];
+}
