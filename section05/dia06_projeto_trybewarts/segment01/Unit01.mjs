83660a2e52b4477cca59ba0212773d1e87ff162e
calebeLAR <ca6ele@gmail.com>
2024-01-16 14:54:40 -0300

    trybe.fundamentos.seção05.dia06.Unit01c.mjs

---
+++ section05/dia06_projeto_trybewarts/segment01/Unit01.mjs
@@ -0,0 +1,13 @@
+export default class Unit01 {
+  static title = 'Projeto Trybewarts';
+  static topics = [
+    'Projeto Trybewarts',
+    'O que vamos fazer?',
+    'Por que isso é importante?',
+    'Especificação',
+    'O que vamos avaliar?',
+    'O que devo desenvolver?',
+    'Entregáveis',
+    'Fique atento e siga as instruções no README.md!',
+  ];
+}
