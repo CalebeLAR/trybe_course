6a8f7a6b9fd98140f7174306968df9a42f9c12d7
calebeLAR <ca6ele@gmail.com>
2024-01-19 11:33:41 -0300

    trybe.fundamentos.seção06.dia01.Unit12c.mjs

---
+++ section06/dia01_fluxo_de_excecoes/segment12/Unit12.mjs
@@ -0,0 +1,7 @@
+export default class Unit12 {
+  static title = 'Referências e Recursos adicionais (Opcional)';
+  static topics = [
+    'Referências e recursos adicionais (opcional)',
+    'Referências',
+  ];
+}
