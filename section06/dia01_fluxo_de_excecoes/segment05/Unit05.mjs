a076888b3a6d8ba88a199ff8e98f034e912bce95
calebeLAR <ca6ele@gmail.com>
2024-01-19 11:33:41 -0300

    trybe.fundamentos.seção06.dia01.Unit05c.mjs

---
+++ section06/dia01_fluxo_de_excecoes/segment05/Unit05.mjs
@@ -0,0 +1,9 @@
+export default class Unit05 {
+  static title = 'Usando o fluxo de exceção';
+  static topics = [
+    'Usando o fluxo de exceção no Trybe Sporting Goods',
+    'Para fixar',
+    'Utilizando o fluxo de exceção no exemplo do Trybe Sporting Goods',
+    'Para praticar, considere o exemplo de um boletim escolar',
+  ];
+}
