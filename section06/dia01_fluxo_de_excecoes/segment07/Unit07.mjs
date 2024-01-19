ab6d3b5dbe0cc3fc31e582ca42b5b2b3cda5dd98
calebeLAR <ca6ele@gmail.com>
2024-01-19 11:33:41 -0300

    trybe.fundamentos.seção06.dia01.Unit07c.mjs

---
+++ section06/dia01_fluxo_de_excecoes/segment07/Unit07.mjs
@@ -0,0 +1,9 @@
+export default class Unit07 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados',
+    'Fixando os aprendizados - Resultado',
+    '83%',
+    '5 de 6 correta(s)',
+  ];
+}
