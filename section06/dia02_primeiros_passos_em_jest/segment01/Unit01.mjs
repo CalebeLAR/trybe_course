0dc8d99f1c7b053615369704c60996ca70eaf64f
calebeLAR <ca6ele@gmail.com>
2024-01-22 15:32:36 -0300

    trybe.fundamentos.seção06.dia02.Unit01c.mjs

---
+++ section06/dia02_primeiros_passos_em_jest/segment01/Unit01.mjs
@@ -0,0 +1,9 @@
+export default class Unit01 {
+  static title = 'Introdução';
+  static topics = [
+    'Onde estamos?',
+    'Por que isso é importante?',
+    'Importância de testes unitários em um cenário real',
+    'Avalie o conteúdo',
+  ];
+}
