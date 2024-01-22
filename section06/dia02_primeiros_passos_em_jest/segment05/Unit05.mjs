ddb3e883eb098a8e22863b722979a425437aa9f9
calebeLAR <ca6ele@gmail.com>
2024-01-22 15:32:36 -0300

    trybe.fundamentos.seção06.dia02.Unit05c.mjs

---
+++ section06/dia02_primeiros_passos_em_jest/segment05/Unit05.mjs
@@ -0,0 +1,13 @@
+export default class Unit05 {
+  static title = 'Instalando e utilizando o Jest';
+  static topics = [
+    'Instalando e utilizando o Jest',
+    'Instalando o Jest',
+    'Passo 1',
+    'Passo 2',
+    'Passo 3',
+    'Passo 4',
+    'Utilizando o Jest',
+    'Passo 1',
+  ];
+}
