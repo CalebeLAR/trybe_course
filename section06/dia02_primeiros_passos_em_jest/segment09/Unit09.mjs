93149b9846469136912bf55aa6188d8f89dc2631
calebeLAR <ca6ele@gmail.com>
2024-01-23 14:29:08 -0300

    trybe.fundamentos.seção06.dia02.Unit09c.mjs

---
+++ section06/dia02_primeiros_passos_em_jest/segment09/Unit09.mjs
@@ -0,0 +1,9 @@
+export default class Unit09 {
+  static title = 'Fixando os Aprendizados';
+  static topics = [
+    'Fixando os aprendizados',
+    'Fixando os aprendizados - Resultado',
+    '89%',
+    '8 de 9 correta(s)',
+  ];
+}
