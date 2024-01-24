a50c5b23112dd64644eefd88470f4b469c7b6be9
calebeLAR <ca6ele@gmail.com>
2024-01-24 11:49:23 -0300

    trybe.fundamentos.seção06.dia03.Unit06c.mjs

---
+++ section06/dia03_matchers_e_cobertura_de_código/segment06/Unit06.mjs
@@ -0,0 +1,9 @@
+export default class Unit06 {
+  static title = 'Fixando os Aprendizados';
+  static topics = [
+    'Fixando os aprendizados',
+    'Fixando os aprendizados - Resultado',
+    '90%',
+    '9 de 10 correta(s)',
+  ];
+}
