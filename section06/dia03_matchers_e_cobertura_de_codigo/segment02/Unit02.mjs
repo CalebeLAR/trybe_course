b1b4d48e6bf39a2ebbddde8842decddb26a10e3c
calebeLAR <ca6ele@gmail.com>
2024-01-24 11:52:04 -0300

    trybe.fundamentos.seção06.dia03.Unit02c.mjs

---
+++ section06/dia03_matchers_e_cobertura_de_codigo/segment02/Unit02.mjs
@@ -0,0 +1,10 @@
+export default class Unit02 {
+  static title = 'Matchers';
+  static topics = [
+    'Matchers para testar arrays, objetos e exceções',
+    'Arrays',
+    'Objetos',
+    'Exceções',
+    'not',
+  ];
+}
