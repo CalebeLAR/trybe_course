0c5d6e9b4046bdc3a3565fe3920445711d0fd8f7
calebeLAR <ca6ele@gmail.com>
2024-01-25 10:15:24 -0300

    trybe.fundamentos.seção06.dia04.Unit01c.mjs

---
+++ section06/dia04_projeto_javascript_testes_unitarios/segment01/Unit01.mjs
@@ -0,0 +1,13 @@
+export default class Unit01 {
+  static title = 'Projeto - JavaScript Testes Unitários';
+  static topics = [
+    'Projeto - JavaScript Testes Unitários',
+    'O que vamos fazer?',
+    'Por que isso é importante?',
+    'Especificação',
+    'O que vamos avaliar?',
+    'O que devo desenvolver?',
+    'Entregáveis',
+    'Fique atento e siga as instruções no README.md do repositório.',
+  ];
+}
