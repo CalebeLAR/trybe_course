2a7eda09b13fc8a09cc5d072aa0121f246f94623
calebeLAR <ca6ele@gmail.com>
2024-01-31 12:27:20 -0300

    trybe.fundamentos.secao07.dia02.Unit10c.mjs

---
+++ section07/dia02_metodos_de_array_filter_e_sort/segment10/Unit10.mjs
@@ -0,0 +1,10 @@
+export default class Unit10 {
+  static title = 'Exercícios - bônus';
+  static topics = [
+    'Exercícios - bônus',
+    'Exercício 1',
+    'Exercício 2',
+    'Exercício 3',
+    'Exercício 4',
+  ];
+}
