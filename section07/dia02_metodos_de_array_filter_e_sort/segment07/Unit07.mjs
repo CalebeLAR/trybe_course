eb463100174950ee308bc2e39f9fd4e839fd8c0f
calebeLAR <ca6ele@gmail.com>
2024-01-31 12:27:19 -0300

    trybe.fundamentos.secao07.dia02.Unit07c.mjs

---
+++ section07/dia02_metodos_de_array_filter_e_sort/segment07/Unit07.mjs
@@ -0,0 +1,9 @@
+export default class Unit07 {
+  static title = 'Array.sort()';
+  static topics = [
+    'Array.sort()',
+    'sort() em arrays de objetos',
+    'Para fixar',
+    'Exercício 1',
+  ];
+}
---
+++ section07/dia02_metodos_de_array_filter_e_sort/segment07/numeric.js
@@ -0,0 +1,5 @@
+const numbers = [2, 5, 10, 1, 8, 23, 4, 3, 1789];
+
+numbers.sort((a, b) => a - b);
+
+console.log(numbers); // [1, 2, 3, 4, 5, 8, 10, 23, 1789]
