387447613357f71697a6afda118145446141a527
calebeLAR <ca6ele@gmail.com>
2024-02-02 12:23:42 -0300

    trybe.fundamentos.secao07.dia01.Unit07c.mjs

---
+++ section07/dia01_metodos_de_array_map/segment07/Unit07.mjs
@@ -0,0 +1,4 @@
+export default class Unit07 {
+  static title = 'Object Property Shorthand';
+  static topics = ['Object Property Shorthand', 'Para Fixar'];
+}
---
+++ section07/dia01_metodos_de_array_map/segment07/parafixar.js
@@ -0,0 +1,17 @@
+/* 
+    Para Fixar
+    Agora é hora de praticar: altere a função getPosition utilizando
+    a property shorthand.
+
+        const getPosition = (latitude, longitude) => ({
+        latitude: latitude,
+        longitude: longitude,
+        });
+*/
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
