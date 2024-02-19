b4e8b088a5a9f84922bfca53ddb96efcd05fea2f
calebeLAR <ca6ele@gmail.com>
2024-02-19 12:58:26 -0300

    trybe.fundamentos.secao07.dia06.Unit06.mjs

---
+++ section05/dia02_bibliotecas_javascript_e_frameworks_css/segment06/Unit06.mjs
@@ -0,0 +1,14 @@
+export default class Unit06 {
+  static title = 'Referência e recursos adicionais';
+  static topics = ['Referências', 'Recursos adicionais'];
+}
+
+
+//  Referências
+//     10 frameworks e bibliotecas JavaScript mais importantes do mercado. Disponível em: https://blog.geekhunter.com.br/frameworks-javascript-e-bibliotecas-java/
+//     As 40 Melhores Bibliotecas e Frameworks JavaScript em 2023. Disponível em: https://kinsta.com/pt/blog/bibliotecas-javascript/
+
+// Recursos adicionais
+//     Tutorial de como instalar e utilizar a biblioteca Just-validate
+//     Tutorial de como instalar e utilizar a biblioteca popup-validation
+
