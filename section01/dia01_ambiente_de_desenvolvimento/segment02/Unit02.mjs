b420c374ba4734eee77a7770e51a119b5e8bc188
calebeLAR <ca6ele@gmail.com>
2024-02-26 19:02:02 -0300

    trybe.front-end.secao01.dia01.Unit02.mjs

---
+++ section01/dia01_ambiente_de_desenvolvimento/segment02/Unit02.mjs
@@ -0,0 +1,12 @@
+export default class Unit02 {
+  static title = 'Configurando o ambiente de desenvolvimento';
+  static topics = [
+    'Configurando o ambiente de desenvolvimento',
+    'O que vamos construir',
+    'Por que criar um ambiente de desenvolvimento web?',
+    'Criando e configurando o projeto',
+    'Iniciando um projeto com NPM',
+    'Configurando Pastas',
+    'Configurando o GIT',
+  ];
+}
