a952a46ca09889779021d007c0e4a1ce2630da47
calebeLAR <ca6ele@gmail.com>
2024-03-04 11:55:23 -0300

    trybe.front-end.secao01.dia02.Unit02.mjs

---
+++ section01/dia02_javascript_assincrono_promises_e_fetch/segment02/Unit02.mjs
@@ -0,0 +1,7 @@
+export default class Unit02 {
+  static title = 'O que é um código assíncrono?';
+  static topics = [
+    'O que é um código assíncrono?',
+    'Por que usar códigos assíncronos',
+  ];
+}
