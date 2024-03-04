48f9cfcf176fd8a8360d959538fde0417f9426af
calebeLAR <ca6ele@gmail.com>
2024-03-04 11:55:23 -0300

    trybe.front-end.secao01.dia02.Unit01.mjs

---
+++ section01/dia02_javascript_assincrono_promises_e_fetch/segment01/Unit01.mjs
@@ -0,0 +1,9 @@
+export default class Unit01 {
+  static title = 'Introdução - JavaScript assíncrono';
+  static topics = [
+    'Introdução - JavaScript assíncrono',
+    'O que vamos aprender hoje',
+    'Você será capaz de:',
+    'Por que isso é importante?',
+  ];
+}
