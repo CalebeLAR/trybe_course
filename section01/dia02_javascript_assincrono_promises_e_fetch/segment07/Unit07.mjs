e3a19dfde6efeafbeb717077ac09ba5680e35609
calebeLAR <ca6ele@gmail.com>
2024-03-09 16:03:16 -0300

    trybe.front-end.secao01.dia02.Unit07.mjs

---
+++ section01/dia02_javascript_assincrono_promises_e_fetch/segment07/Unit07.mjs
@@ -0,0 +1,10 @@
+export default class Unit07 {
+  static title = 'Trabalhando com várias promises';
+  static topics = [
+    'Trabalhando com várias promises',
+    'Método Promise.all',
+    'Método Promise.race',
+    'Método Promise.any',
+    'Método Promise.allSettled',
+  ];
+}
