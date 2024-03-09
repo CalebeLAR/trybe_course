33538552b15609f6e1a0727a965f555868e76f86
calebeLAR <ca6ele@gmail.com>
2024-03-09 16:03:16 -0300

    trybe.front-end.secao01.dia02.Unit09.mjs

---
+++ section01/dia02_javascript_assincrono_promises_e_fetch/segment09/Unit09.mjs
@@ -0,0 +1,8 @@
+export default class Unit09 {
+  static title = 'Exercícios - agora, a prática';
+  static topics = [
+    'Exercícios - agora, a prática',
+    'API de Super Heróis',
+    'Select Pet',
+  ];
+}
