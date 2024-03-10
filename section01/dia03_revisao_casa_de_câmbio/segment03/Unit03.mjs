a9a11cfb2f1e365a9fa4be4f666f05d225ab58fa
calebeLAR <ca6ele@gmail.com>
2024-03-10 13:24:00 -0300

    trybe.front-end.secao01.dia03.Unit03.mjs

---
+++ section01/dia03_revisao_casa_de_câmbio/segment03/Unit03.mjs
@@ -0,0 +1,9 @@
+export default class Unit03 {
+  static title = 'Exercício do dia';
+  static topics = [
+    'Exercício do dia',
+    'Exercício - Casa de Câmbio',
+    'Protótipo do Figma',
+    'Instruções',
+  ];
+}
