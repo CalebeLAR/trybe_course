24f815612ef77ef0aca7f72681790cb5b50d1afa
calebeLAR <ca6ele@gmail.com>
2024-03-11 12:07:50 -0300

    trybe.front-end.secao01.dia04.Unit08.mjs

---
+++ section01/dia04_async_await_e_testes_assincronos/segment08/Unit08.mjs
@@ -0,0 +1,22 @@
+export default class Unit08 {
+    static title = 'Recursos adicionais (opcional)';
+    static topics = ['Recursos adicionais (opcional)'];
+}
+
+// Jest - Testing Asynchronous Code - Video
+// https://www.youtube.com/watch?v=Y4PHrT6Cc_A
+
+// Jest - Testing Asynchronous Code - Leitura
+// https://deltice.github.io/jest/docs/pt-BR/asynchronous.html#content
+
+// Testing Asynchronous Code
+// https://jestjs.io/docs/asynchronous
+
+// An Async Example
+// https://jestjs.io/docs/tutorial-async
+
+// Setup and Teardown
+// https://jestjs.io/docs/setup-teardown
+
+// Jest - Setup and Teardown - Video
+// https://www.youtube.com/watch?v=wWx9uZa-Wnk
