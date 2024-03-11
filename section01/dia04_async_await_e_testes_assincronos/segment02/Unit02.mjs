5caecd2bdb87b8b57d570e87222e6b3c4550619b
calebeLAR <ca6ele@gmail.com>
2024-03-11 12:07:50 -0300

    trybe.front-end.secao01.dia04.Unit02.mjs

---
+++ section01/dia04_async_await_e_testes_assincronos/segment02/Unit02.mjs
@@ -0,0 +1,11 @@
+export default class Unit02 {
+    static title = 'Async e Await';
+    static topics = [
+        'Async e Await',
+        'O que vamos construir hoje',
+        'Buscando dados com uma API de CEP',
+        'O funcionamento do operador await',
+        'Funções Assíncronas',
+        'Lidando com Erros',
+    ];
+}
