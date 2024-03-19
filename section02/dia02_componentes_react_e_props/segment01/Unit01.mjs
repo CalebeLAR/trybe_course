b3fdd2093941f158445144670ae3c43f931b84b3
calebeLAR <ca6ele@gmail.com>
2024-03-19 14:50:28 -0300

    trybe.front-end.secao02.dia02.Unit01.mjs

---
+++ section02/dia02_componentes_react_e_props/segment01/Unit01.mjs
@@ -0,0 +1,9 @@
+export default class Unit01 {
+  static title = 'Introdução';
+  static topics = [
+    'Introdução',
+    'O que você vai aprender?',
+    'Você será capaz de:',
+    'Por que isso é importante?',
+  ];
+}
