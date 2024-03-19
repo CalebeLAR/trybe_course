0296a7bcc302556ecd4998bad76ce8541d8a9525
calebeLAR <ca6ele@gmail.com>
2024-03-18 21:12:27 -0300

    trybe.front-end.secao02.dia01.Unit08.mjs

---
+++ section02/dia01_introducao_ao_react_e_ao_typescript/segment08/Unit08.mjs
@@ -0,0 +1,7 @@
+export default class Unit08 {
+  static title = 'Exercícios - agora, a prática';
+  static topics = [
+    'Exercícios – Agora a prática',
+    '🚀 Criando um projeto React com Vite e TypeScript',
+  ];
+}
