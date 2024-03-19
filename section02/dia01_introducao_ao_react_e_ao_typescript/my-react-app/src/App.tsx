6e16a4281e022c187faff399801516d96ddb0adc
calebeLAR <ca6ele@gmail.com>
2024-03-18 21:12:59 -0300

    update.segment05: componentizando

---
+++ section02/dia01_introducao_ao_react_e_ao_typescript/my-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <h1>epp</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
