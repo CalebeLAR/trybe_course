27c44f3a500102728761ffba26cc9fe482977380
calebeLAR <ca6ele@gmail.com>
2024-03-20 18:10:52 -0300

    stackBlitz: renderização condicional

--- section02/dia02_componentes_react_e_props/react-exercise/src/components/LuckyNumbers.tsx
+++ section02/dia02_componentes_react_e_props/react-exercise/src/components/LuckyNumbers.tsx
@@ -3,17 +3,23 @@ function LuckyNumbers() {
     return Math.floor(Math.random() * 60 + 1);
   }
 
+  const show = true;
+
   return (
     <>
-      <h2 className="subtitle">Seus números da sorte da Mega-Sena são:</h2>
-      <ul>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-      </ul>
+      <h2 className='subtitle'>Seus números da sorte da Mega-Sena são:</h2>
+      {show ? (
+        <ul>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+        </ul>
+      ) : (
+        <h3>Não há números para serem exibidos 😢</h3>
+      )}
     </>
   );
 }
