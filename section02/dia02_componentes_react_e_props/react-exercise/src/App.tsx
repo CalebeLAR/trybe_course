140c5e00a59eec668cc23252d303f63b78ad5256
calebeLAR <ca6ele@gmail.com>
2024-03-20 18:41:24 -0300

    stackBlitz: renderização de listas

--- section02/dia02_componentes_react_e_props/react-exercise/src/App.tsx
+++ section02/dia02_componentes_react_e_props/react-exercise/src/App.tsx
@@ -2,12 +2,14 @@ import Greeting from './components/Greeting';
 import LuckyNumbers from './components/LuckyNumbers';
 
 import './App.css';
+import PastResults from './components/PastResults';
 
 function App() {
   return (
     <>
       <Greeting person={{ firstName: 'Chapolin', lastName: 'Colorado' }} />
       <LuckyNumbers />
+      <PastResults />
     </>
   );
 }
