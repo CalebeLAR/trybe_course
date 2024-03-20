cf5b7f9cab123a87bc6c441f779f403c977f6f94
calebeLAR <ca6ele@gmail.com>
2024-03-20 19:08:57 -0300

    trybe.front-end.secao02.dia02.Unit05.mjs

---
+++ section02/dia02_componentes_react_e_props/segment05/Unit05.mjs
@@ -0,0 +1,6 @@
+export default class Unit05 {
+  static title = 'Renderização de listas';
+  static topics = ['Renderização de listas'];
+  static videos = ['FE 2.2 - Renderização de Listas'];
+  static stackblitz = ['my-react-app', 'react-exercise'];
+}
