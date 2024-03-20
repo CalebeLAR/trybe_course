d095cee65c07efc3f153bf7463c5c7c706dc2b5b
calebeLAR <ca6ele@gmail.com>
2024-03-20 18:21:22 -0300

    update: Renderização de listas

--- section02/dia02_componentes_react_e_props/my-react-app/src/App.tsx
+++ section02/dia02_componentes_react_e_props/my-react-app/src/App.tsx
@@ -2,44 +2,47 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Book from './components/Book';
 
-const book1 = {
-  title: 'The GrandPhater',
-  pages: 433,
-  isRecord: true,
-  isFavorite: false,
-};
-
-const book2 = {
-  title: 'Journey Through the Stars',
-  pages: 289,
-  isRecord: false,
-  isFavorite: true,
-};
-
-const book3 = {
-  title: 'Mysteries of the Ancient World',
-  pages: 501,
-  isRecord: true,
-  isFavorite: true,
-};
-
-const book4 = {
-  title: 'The Silent Language of Plants',
-  pages: 324,
-  isRecord: false,
-  isFavorite: false,
-};
-
+const books = [
+  {
+    title: 'The GrandPhater',
+    pages: 433,
+    isRecord: true,
+    isFavorite: false,
+  },
+  {
+    title: 'The GrandPhater 2',
+    pages: 433,
+    isRecord: true,
+    isFavorite: false,
+  },
+  {
+    title: 'Journey Through the Stars',
+    pages: 289,
+    isRecord: false,
+    isFavorite: true,
+  },
+  {
+    title: 'Mysteries of the Ancient World',
+    pages: 501,
+    isRecord: true,
+    isFavorite: true,
+  },
+  {
+    title: 'The Silent Language of Plants',
+    pages: 324,
+    isRecord: false,
+    isFavorite: false,
+  },
+];
 function App() {
   return (
     <div>
       <Header />
       <p>Meus livros: </p>
       <ul>
-        <Book book={ book1 } />
-        <Book book={ book2 } />
-        <Book book={ book3 } />
-        <Book book={ book4 } />
+        {books.map((book) => (
+          <Book key={ book.title } book={ book } />
+        ))}
       </ul>
       <Footer />
     </div>
