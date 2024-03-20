320d898e9e76b85991f5a1fd6cb0649a7e9c7926
calebeLAR <ca6ele@gmail.com>
2024-03-20 19:08:57 -0300

    trybe.front-end.secao02.dia02.Unit04.mjs

---
+++ section02/dia02_componentes_react_e_props/segment04/Unit04.mjs
@@ -0,0 +1,11 @@
+export default class Unit04 {
+  static title = 'Renderização condicional';
+  static topics = [
+    'Renderização condicional',
+    'Operador Logical AND - &&',
+    'Operador ternário - cond ? <A /> : <B />',
+    'if/else',
+  ];
+  static videos = ['FE 2.2 - Renderização condicional'];
+  static stackblitz = ['react-exercise'];
+}
