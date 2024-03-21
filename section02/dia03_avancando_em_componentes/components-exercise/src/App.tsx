bc3ab96db5c66c779d2bbbdd02eecea80ed60b88
calebeLAR <ca6ele@gmail.com>
2024-03-21 17:53:19 -0300

    images

--- section02/dia03_avancando_em_componentes/components-exercise/src/App.tsx
+++ section02/dia03_avancando_em_componentes/components-exercise/src/App.tsx
@@ -1,9 +1,12 @@
 import Button from './components/Button';
+import Image from './components/Image';
 
 function App() {
   return (
     <div className="card logo react">
       <Button>{'I\'m a Button!'}</Button>
+      <br />
+      <Image />
     </div>
   );
 }
