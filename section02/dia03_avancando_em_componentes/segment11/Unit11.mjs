22e4de474b62c73552689e018f9eeb67367c7e08
calebeLAR <ca6ele@gmail.com>
2024-03-22 18:09:59 -0300

    trybe.front-end.secao02.dia03.Unit11.mjs

---
+++ section02/dia03_avancando_em_componentes/segment11/Unit11.mjs
@@ -0,0 +1,9 @@
+export default class Unit11 {
+  static title = 'Exercícios - teste seu conhecimento';
+  static topics = [
+    'Exercícios - Teste seus conhecimentos',
+    '🚀 Products list - React Components',
+  ];
+  static videos = [];
+  static stackblitz = [];
+}
