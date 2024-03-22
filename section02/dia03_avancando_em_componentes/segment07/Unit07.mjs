e8526886b626acfcc354f7f3156461f198a38833
calebeLAR <ca6ele@gmail.com>
2024-03-22 18:09:59 -0300

    trybe.front-end.secao02.dia03.Unit07.mjs

---
+++ section02/dia03_avancando_em_componentes/segment07/Unit07.mjs
@@ -0,0 +1,9 @@
+export default class Unit07 {
+  static title = 'Enviando argumentos para o event handler';
+  static topics = ['Enviando argumentos para o event handler'];
+  static videos = [
+    'FE 3.1 - Introdução a generics em Typescript',
+    'FE 3.1 - Passando parâmetros para o event handler',
+  ];
+  static stackblitz = ['components-exercise'];
+}
