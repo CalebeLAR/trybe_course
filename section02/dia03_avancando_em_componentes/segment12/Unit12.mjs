8803ad03bd6989654bd1459386551e27ab84150f
calebeLAR <ca6ele@gmail.com>
2024-03-22 18:09:59 -0300

    trybe.front-end.secao02.dia03.Unit12.mjs

---
+++ section02/dia03_avancando_em_componentes/segment12/Unit12.mjs
@@ -0,0 +1,9 @@
+export default class Unit12 {
+  static title = 'Recursos adicionais';
+  static topics = ['Recursos adicionais'];
+  static videos = [];
+  static stackblitz = [];
+}
+
+// Adicionando animações com Framer Motion
+// https://www.framer.com/motion/
