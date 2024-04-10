7e2e51e5eb9f5825036e18b3c59c6efd45397223
calebeLAR <ca6ele@gmail.com>
2024-04-10 20:09:22 -0300

    trybe.front-end.secao02.dia05.Unit04.mjs

---
+++ section02/dia05_estados_e_eventos/segment04/Unit04.mjs
@@ -0,0 +1,10 @@
+export default class Unit04 {
+  static title = 'Criando estado em um componente';
+  static topics = [
+    'Criando estado em um componente',
+    'React Dev Tools',
+    'Criando o primeiro componente com estado',
+    'Atualizando múltiplas vezes o mesmo estado',
+  ];
+  static videos = ['FE 3.1 - useState'];
+}
