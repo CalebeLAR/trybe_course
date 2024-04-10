50b05693bfb385e7be5282b183359c1db09c2e1d
calebeLAR <ca6ele@gmail.com>
2024-04-10 20:09:22 -0300

    trybe.front-end.secao02.dia05.Unit07.mjs

---
+++ section02/dia05_estados_e_eventos/segment07/Unit07.mjs
@@ -0,0 +1,8 @@
+export default class Unit07 {
+  static title = 'Criando estados com tipos genéricos';
+  static topics = [
+    'Criando estados com tipos genéricos',
+    'Generics no Typescript',
+  ];
+  static videos = ["FE 3.1 - Tipando estado com Generics"];
+}
