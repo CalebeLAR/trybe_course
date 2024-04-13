c0897bf6cc161bb87fac419ae15363a665961257
calebeLAR <ca6ele@gmail.com>
2024-04-13 20:19:09 -0300

    trybe.front-end.secao02.dia06.Unit06.mjs

---
+++ section02/dia06_formularios/segment06/Unit06.mjs
@@ -0,0 +1,5 @@
+export default class Unit06 {
+  static title = 'Elevação de estado';
+  static topics = ['Elevação de estado', 'Passo 1', 'Passo 2'];
+  static videos = ['[FE] Elevação de Estado'];
+}
