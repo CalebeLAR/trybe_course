487e2fae71998ab129f11f8555b30f5df7938edc
calebeLAR <ca6ele@gmail.com>
2024-04-13 20:19:09 -0300

    trybe.front-end.secao02.dia06.Unit07.mjs

---
+++ section02/dia06_formularios/segment07/Unit07.mjs
@@ -0,0 +1,8 @@
+export default class Unit07 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '100%',
+    '10 de 10 correta(s)',
+  ];
+}
