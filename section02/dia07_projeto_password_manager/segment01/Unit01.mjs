c0963f2b240f302cade269a00aeb903b89b63f23
calebeLAR <ca6ele@gmail.com>
2024-04-23 15:26:58 -0300

    trybe.front-end.secao02.dia07.Unit01.mjs

---
+++ section02/dia07_projeto_password_manager/segment01/Unit01.mjs
@@ -0,0 +1,13 @@
+export default class Unit01 {
+  static title = 'Projeto - Password Manager';
+  // SOLAR SYSTEM ANTIGO SOLAR SYSTEM
+  static topics = [
+    'Projeto - Password Manager',
+    'O que vamos fazer?',
+    'Por que isso é importante?',
+    'Especificação',
+    'O que vamos avaliar?',
+    'O que devo desenvolver?',
+    'Entregáveis',
+  ];
+}
