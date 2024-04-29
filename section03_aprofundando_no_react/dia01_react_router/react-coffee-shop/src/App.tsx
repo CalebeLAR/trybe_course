75cf0093c051de5ac5421d3f1849cf5b4af656c6
calebeLAR <ca6ele@gmail.com>
2024-04-29 16:54:38 -0300

    segment08

--- section03_aprofundando_no_react/dia01_react_router/react-coffee-shop/src/App.tsx
+++ section03_aprofundando_no_react/dia01_react_router/react-coffee-shop/src/App.tsx
@@ -3,18 +3,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import CoffeeList from './pages/coffee-list';
-import NavBar from './components/nav-bar'
 import Coffee from './pages/coffee';
 import NotFound from './pages/not-found';
+import Layout from './components/layout';
 
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coffees" element={<CoffeeList />} />
-        <Route path="/coffees/:coffee" element={<Coffee />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/coffees" element={<CoffeeList />} />
+          <Route path="/coffees/:coffee" element={<Coffee />} />
+        </Route>
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
