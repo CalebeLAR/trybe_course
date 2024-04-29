1ab32a05ddd1effa87d4a577d78cc8cd5e600968
calebeLAR <ca6ele@gmail.com>
2024-04-29 16:55:25 -0300

    trybe.front-end.secao03.dia01.Unit09.mjs

---
+++ section03_aprofundando_no_react/dia01_react_router/segment09/Unit09.mjs
@@ -0,0 +1,10 @@
+export default class Unit09 {
+  static title = 'Hook useLocation';
+  static topics = [
+    'Hook useLocation',
+    'Enviando informações por meio das rotas',
+    'Enviando informações por meio do componente Link',
+    'Enviando informações por meio do useNavigate',
+  ];
+  static videos = [];
+}
