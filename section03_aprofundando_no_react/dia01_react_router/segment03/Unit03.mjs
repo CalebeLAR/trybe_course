edc15e12c1e6892f19300e080caa9a498bc6987f
calebeLAR <ca6ele@gmail.com>
2024-04-29 16:55:24 -0300

    trybe.front-end.secao03.dia01.Unit03.mjs

---
+++ section03_aprofundando_no_react/dia01_react_router/segment03/Unit03.mjs
@@ -0,0 +1,13 @@
+export default class Unit03 {
+  static title = 'Criando Rotas';
+  static topics = [
+    'Criando rotas',
+    'Passo 1 - Instalando o React Router',
+    'Passo 2 - Componente BrowserRouter',
+    'Diretamente no componente App',
+    'Envolvendo a aplicação no arquivo main.tsx',
+    'Passo 3 - Criando as páginas da aplicação',
+    'Passo 4 - Componentes Routes e Route',
+  ];
+  static videos = ['[FE 4.1] - Configurando Rotas'];
+}
