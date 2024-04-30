861b29a10e4def5b1f5817878dcd34de1fcf255f
calebeLAR <ca6ele@gmail.com>
2024-04-30 16:21:23 -0300

    trybe.front-end.secao03.dia02.Unit09.mjs

---
+++ section03_aprofundando_no_react/dia02_useeffect/segment09/Unit09.mjs
@@ -0,0 +1,9 @@
+export default class Unit09 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '88%',
+    '7 de 8 correta(s)',
+  ];
+  static videos = [];
+}
