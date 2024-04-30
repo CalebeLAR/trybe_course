56d5e7a86ebfb7f1e8d33fe541dc133f01146169
calebeLAR <ca6ele@gmail.com>
2024-04-30 16:21:23 -0300

    trybe.front-end.secao03.dia02.Unit08.mjs

---
+++ section03_aprofundando_no_react/dia02_useeffect/segment08/Unit08.mjs
@@ -0,0 +1,9 @@
+export default class Unit08 {
+  static title = 'Evitando efeitos desnecessários';
+  static topics = [
+    'Evitando efeitos desnecessários',
+    'Elementos primitivos',
+    'Elementos primitivos na lista de dependências',
+  ];
+  static videos = ['[FE] 3.2 - Rolagem Infinita com useEffect'];
+}
