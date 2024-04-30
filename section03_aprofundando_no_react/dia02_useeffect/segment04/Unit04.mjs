f4fe7f7c7e5714863f56de96a864ea31f82fa6ff
calebeLAR <ca6ele@gmail.com>
2024-04-30 16:21:23 -0300

    trybe.front-end.secao03.dia02.Unit04.mjs

---
+++ section03_aprofundando_no_react/dia02_useeffect/segment04/Unit04.mjs
@@ -0,0 +1,12 @@
+export default class Unit04 {
+  static title = 'Hook useEffect';
+  static topics = [
+    'Hook useEffect',
+    'useEffect sem segundo argumento',
+    'useEffect com o segundo argumento sendo um array vazio',
+    'useEffect com o segundo argumento sendo um array com itens',
+    'useEffect com a callback retornando uma função',
+    'Resumindo',
+  ];
+  static videos = ['[FE] 3.2 - Primeiros passos com useEffect'];
+}
