4775dc71d45161187832a6af0a4501493a99870e
calebeLAR <ca6ele@gmail.com>
2024-04-30 16:21:23 -0300

    trybe.front-end.secao03.dia02.Unit05.mjs

---
+++ section03_aprofundando_no_react/dia02_useeffect/segment05/Unit05.mjs
@@ -0,0 +1,12 @@
+export default class Unit05 {
+  static title = 'useEffect na prática';
+  static topics = [
+    'useEffect na prática',
+    'Criando a função fetch',
+    'Implementando a aplicação',
+    'Passo 1',
+    'Passo 2',
+    'Passo 3',
+  ];
+  static videos = [];
+}
