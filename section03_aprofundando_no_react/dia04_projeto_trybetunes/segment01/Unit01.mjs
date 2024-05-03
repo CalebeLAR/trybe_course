1a04313e0df26386f65a0f8ffed9f2d7d52e0070
calebeLAR <ca6ele@gmail.com>
2024-05-03 15:10:06 -0300

    trybe.front-end.secao03.dia04.Unit01.mjs

---
+++ section03_aprofundando_no_react/dia04_projeto_trybetunes/segment01/Unit01.mjs
@@ -0,0 +1,13 @@
+export default class Unit01 {
+  static title = 'Projeto - Trybetunes';
+  static topics = [
+    'Projeto - Trybe Tunes',
+    'O que vamos fazer?',
+    'Por que isso é importante?',
+    'Especificação',
+    'O que vamos avaliar?',
+    'O que devo desenvolver?',
+    'Entregáveis',
+    'Fique atento e siga as instruções no README.md do repositório! 🧐',
+  ];
+}
