95e892270e039a43317ac193f1e88e4c8a7aaf1d
calebeLAR <ca6ele@gmail.com>
2024-05-06 16:21:30 -0300

    trybe.front-end.secao04.dia01.Unit02.mjs

---
+++ section04_testes_automatizados_com_react_testing_library/dia01_introducao_a_react_testing_library/segment02/Unit02.mjs
@@ -0,0 +1,8 @@
+export default class Unit02 {
+  static title = 'Conceitos importantes';
+  static topics = [
+    'Conceitos importantes',
+    'Cobertura de código e Cobertura com testes automatizados',
+    'Casos de uso x Cobertura de código',
+  ];
+}
