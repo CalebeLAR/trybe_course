1e160fa888e618ffe29dbb8e2ce55c5fda827ed5
calebeLAR <ca6ele@gmail.com>
2024-05-08 17:13:15 -0300

    trybe.front-end.secao04.dia01.Unit09.mjs

---
+++ section04_testes_automatizados_com_react_testing_library/dia01_introducao_a_react_testing_library/segment09/Unit09.mjs
@@ -0,0 +1,9 @@
+export default class Unit09 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '80%',
+    '8 de 10 correta(s)',
+    'Avalie o conteúdo',
+  ];
+}
