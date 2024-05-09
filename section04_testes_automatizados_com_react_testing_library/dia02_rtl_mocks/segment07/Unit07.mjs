c767c2bd67b5f0f480817e4567fd2a8cf401d19b
calebeLAR <ca6ele@gmail.com>
2024-05-09 17:05:46 -0300

    trybe.front-end.secao04.dia02.Unit07.mjs

---
+++ section04_testes_automatizados_com_react_testing_library/dia02_rtl_mocks/segment07/Unit07.mjs
@@ -0,0 +1,8 @@
+export default class Unit07 {
+  static title = 'Testando APIs e inputs';
+  static topics = [
+    'Testando APIs e inputs',
+    'Mock do fetch com spyOn',
+    'Mock do fetch com fn',
+  ];
+}
