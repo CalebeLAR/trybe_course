fc39b249ba0481dda16076f5668887ecbc5c1beb
calebeLAR <ca6ele@gmail.com>
2024-05-09 17:05:46 -0300

    trybe.front-end.secao04.dia02.Unit03.mjs

---
+++ section04_testes_automatizados_com_react_testing_library/dia02_rtl_mocks/segment03/Unit03.mjs
@@ -0,0 +1,5 @@
+export default class Unit03 {
+  static title = 'Mockando funções com Vitest';
+  static topics = ['Mockando funções com Vitest'];
+  static videos = ['[FE] 4.2 - Primeiro Mock com Vitest'];
+}
