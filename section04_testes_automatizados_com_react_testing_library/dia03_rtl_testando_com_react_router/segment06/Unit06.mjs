5d54f1c005c984c70755f0eef94870d06b9e18b3
calebeLAR <ca6ele@gmail.com>
2024-05-10 19:04:32 -0300

    trybe.front-end.secao04.dia03.Unit06.mjs

---
+++ section04_testes_automatizados_com_react_testing_library/dia03_rtl_testando_com_react_router/segment06/Unit06.mjs
@@ -0,0 +1,9 @@
+export default class Unit06 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '90%',
+    '9 de 10 correta(s)',
+  ];
+  static videos = [];
+}
