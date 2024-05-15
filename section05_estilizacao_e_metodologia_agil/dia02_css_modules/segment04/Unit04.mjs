a7eeb8cfb2fc6201ef603e5ac6ec13dee73f85b9
calebeLAR <ca6ele@gmail.com>
2024-05-15 15:19:48 -0300

    trybe.front-end.secao05.dia02.Unit04.mjs

---
+++ section05_estilizacao_e_metodologia_agil/dia02_css_modules/segment04/Unit04.mjs
@@ -0,0 +1,6 @@
+export default class Unit04 {
+  static title = 'Aula ao vivo';
+  static topics = ['Aula ao vivo'];
+  static aula = 'Aula - FE 5.1 - CSS Modules - Noite';
+  static professor = 'Tiago dos Santos Quadros';
+}
