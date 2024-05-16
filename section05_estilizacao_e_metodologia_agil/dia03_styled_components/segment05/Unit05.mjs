130a223e46d3cb9da7017a6ee9ec7b37ae33a9b3
calebeLAR <ca6ele@gmail.com>
2024-05-16 16:09:29 -0300

    trybe.front-end.secao05.dia03.Unit05.mjs

---
+++ section05_estilizacao_e_metodologia_agil/dia03_styled_components/segment05/Unit05.mjs
@@ -0,0 +1,9 @@
+export default class Unit05 {
+  static title = 'Pseudo-classes e aninhamento';
+  static topics = [
+    'Pseudo-classes e aninhamento',
+    'Aninhamento de classes',
+    'Componentes com pseudo-classes',
+  ];
+  static videos = [];
+}
