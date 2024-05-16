e781e2bcff346dcb35a6095c4005e3dc73ac4057
calebeLAR <ca6ele@gmail.com>
2024-05-16 16:09:29 -0300

    trybe.front-end.secao05.dia03.Unit02.mjs

---
+++ section05_estilizacao_e_metodologia_agil/dia03_styled_components/segment02/Unit02.mjs
@@ -0,0 +1,10 @@
+export default class Unit02 {
+  static title = 'Styled Components';
+  static topics = [
+    'Styled Components',
+    'Instalando Styled Components',
+    'Criando um componente estilizado',
+    'Extraindo os componentes estilizados em um arquivo separado',
+  ];
+  static videos = [];
+}
