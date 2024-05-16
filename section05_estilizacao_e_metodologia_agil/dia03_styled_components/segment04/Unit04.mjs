baded43b93dff7f0e0bb89a4d63e2d9ccda3c503
calebeLAR <ca6ele@gmail.com>
2024-05-16 16:09:29 -0300

    trybe.front-end.secao05.dia03.Unit04.mjs

---
+++ section05_estilizacao_e_metodologia_agil/dia03_styled_components/segment04/Unit04.mjs
@@ -0,0 +1,8 @@
+export default class Unit04 {
+  static title = 'Utilizando props em um Styled Component';
+  static topics = [
+    'Utilizando props em um Styled Component',
+    'Passando props para um Styled Component',
+  ];
+  static videos = [];
+}
