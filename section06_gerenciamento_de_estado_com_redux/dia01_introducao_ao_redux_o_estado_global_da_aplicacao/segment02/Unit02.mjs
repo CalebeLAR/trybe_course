33e81a861d75079ae126b22fe780856f9ddc3f7e
calebeLAR <ca6ele@gmail.com>
2024-05-20 16:04:56 -0300

    trybe.front-end.secao06.dia01.Unit02.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia01_introducao_ao_redux_o_estado_global_da_aplicacao/segment02/Unit02.mjs
@@ -0,0 +1,15 @@
+export default class Unit02 {
+  static title = 'Peças do Redux';
+  static topics = [
+    'Peças do Redux',
+    'Criando o estado global - Store',
+    'Reducer',
+    'Actions',
+    'dispatch()',
+    'getState()',
+    'subscribe()',
+    'Juntando todas as peças',
+    'E o Redux Toolkit (RTK)?',
+  ];
+  static videos = ['[FE] 6.1 - Peças do Redux'];
+}
