c53e8e8a4bb7f5d30d80da7225b36d20e899187a
calebeLAR <ca6ele@gmail.com>
2024-05-21 16:55:57 -0300

    trybe.front-end.secao06.dia02.Unit01.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia02_usando_o_redux_no_react/segment01/Unit01.mjs
@@ -0,0 +1,10 @@
+export default class Unit01 {
+  static title = 'Introdução – Redux com React';
+  static topics = [
+    'Introdução – Redux com React',
+    'O que vamos aprender?',
+    'Você será capaz de:',
+    'Por que isso é importante?',
+  ];
+  static videos = [];
+}
