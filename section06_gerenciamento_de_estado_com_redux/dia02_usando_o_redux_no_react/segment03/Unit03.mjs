2e4b30988446a345a0cc6b6500d579a7da9f4a96
calebeLAR <ca6ele@gmail.com>
2024-05-21 16:55:57 -0300

    trybe.front-end.secao06.dia02.Unit03.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia02_usando_o_redux_no_react/segment03/Unit03.mjs
@@ -0,0 +1,9 @@
+export default class Unit03 {
+  static title = 'Configurando Redux com React';
+  static topics = [
+    'Configurando Redux com React',
+    'Criando a Store e o Reducer',
+    'Provider',
+  ];
+  static videos = ['[FE] 6.2 - Configurando o Redux com React'];
+}
