e19a2be61dcc9961bb2caedc5b80a5d03168bf54
calebeLAR <ca6ele@gmail.com>
2024-05-21 16:55:58 -0300

    trybe.front-end.secao06.dia02.Unit08.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia02_usando_o_redux_no_react/segment08/Unit08.mjs
@@ -0,0 +1,9 @@
+export default class Unit08 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '100%',
+    '8 de 8 correta(s)',
+  ];
+  static videos = [];
+}
