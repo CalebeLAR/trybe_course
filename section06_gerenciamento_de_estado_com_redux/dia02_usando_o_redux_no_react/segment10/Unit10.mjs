5edc68bf0226205918053a7246d14ca10ffc8339
calebeLAR <ca6ele@gmail.com>
2024-05-21 16:55:58 -0300

    trybe.front-end.secao06.dia02.Unit10.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia02_usando_o_redux_no_react/segment10/Unit10.mjs
@@ -0,0 +1,8 @@
+export default class Unit10 {
+  static title = 'Exercícios – Agora, a prática';
+  static topics = [
+    'Exercícios – Agora, a prática',
+    'Exercício – Redux com React',
+  ];
+  static videos = [];
+}
