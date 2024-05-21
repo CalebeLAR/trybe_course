19138def478bc915261ba7b25712820d8938bca8
calebeLAR <ca6ele@gmail.com>
2024-05-21 16:55:58 -0300

    trybe.front-end.secao06.dia02.Unit07.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia02_usando_o_redux_no_react/segment07/Unit07.mjs
@@ -0,0 +1,6 @@
+export default class Unit07 {
+  static title = 'Utilizando mais de um Reducer';
+  static topics = ['Utilizando mais de um Reducer', 'combineReducers'];
+  static videos = [];
+}
+v
