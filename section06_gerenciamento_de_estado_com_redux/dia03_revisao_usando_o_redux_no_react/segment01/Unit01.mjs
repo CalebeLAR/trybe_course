83d38bf677e3bd344d5c90511d1ab7f35567a065
calebeLAR <ca6ele@gmail.com>
2024-05-22 14:44:42 -0300

    trybe.front-end.secao06.dia03.Unit01.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia03_revisao_usando_o_redux_no_react/segment01/Unit01.mjs
@@ -0,0 +1,10 @@
+export default class Unit01 {
+  static title = 'Introdução – React e Redux na prática';
+  static topics = [
+    'Introdução – React e Redux na prática',
+    'O que vamos aprender?',
+    'Você será capaz de:',
+    'Por que isso é importante?',
+  ];
+  static videos = [];
+}
