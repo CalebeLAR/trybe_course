301e362bbfeea85bb6818d0de61540f05ea1e477
calebeLAR <ca6ele@gmail.com>
2024-05-22 14:44:42 -0300

    trybe.front-end.secao06.dia03.Unit03.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia03_revisao_usando_o_redux_no_react/segment03/Unit03.mjs
@@ -0,0 +1,8 @@
+export default class Unit03 {
+  static title = 'Exercício do dia';
+  static topics = [
+    'Exercício do dia',
+    'Exercício – Formulário com Redux e React',
+  ];
+  static videos = [];
+}
