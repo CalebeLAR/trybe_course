32223900e491560b9da3fe0f13fd519834ac3634
calebeLAR <ca6ele@gmail.com>
2024-05-23 16:46:34 -0300

    redux-thank

---
+++ section06_gerenciamento_de_estado_com_redux/app-redux-thank/src/types.ts
@@ -0,0 +1,11 @@
+// src/types.ts
+import { ThunkDispatch } from 'redux-thunk';
+import { UnknownAction } from 'redux';
+
+export type ReduxState = {
+  isFetching: boolean,
+  imageURL: string,
+  errorMessage: string,
+};
+
+export type Dispatch = ThunkDispatch<ReduxState, null, UnknownAction>;
