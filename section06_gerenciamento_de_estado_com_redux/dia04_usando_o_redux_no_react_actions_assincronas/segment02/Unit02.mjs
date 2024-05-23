460431fee4addee27fc43c83013227cf242175fb
calebeLAR <ca6ele@gmail.com>
2024-05-23 17:00:49 -0300

    trybe.front-end.secao06.dia04.Unit02.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia04_usando_o_redux_no_react_actions_assincronas/segment02/Unit02.mjs
@@ -0,0 +1,5 @@
+export default class Unit02 {
+  static title = 'Redux-thunk';
+  static topics = ['Redux-thunk', 'Relembrando o fluxo do Redux'];
+  static videos = ['FE07.4 Introdução ao Redux Thunk'];
+}
