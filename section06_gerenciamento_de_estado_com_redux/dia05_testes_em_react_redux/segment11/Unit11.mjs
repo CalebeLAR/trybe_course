c4fb55dd200576147d6b17922700b99b74763dc5
calebeLAR <ca6ele@gmail.com>
2024-05-24 17:02:39 -0300

    trybe.front-end.secao06.dia05.Unit11.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia05_testes_em_react_redux/segment11/Unit11.mjs
@@ -0,0 +1,13 @@
+export default class Unit11 {
+  static title = 'Recursos adicionais (opcional)';
+  static topics = ['Recursos adicionais (opcional)'];
+  static videos = [];
+}
+
+
+// Custom rendering in RTL with multiple Providers
+// https://medium.com/nmc-techblog/custom-rendering-in-react-testing-library-done-right-e260e01ba6f7
+// Writing Tests
+// https://redux.js.org/usage/writing-tests
+// Writing Jest tests for a Redux Toolkit Slice
+// https://dev.to/bionicjulia/writing-jest-tests-for-a-redux-toolkit-slice-3co3
