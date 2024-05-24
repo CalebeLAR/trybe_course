a91709e3a015933fde6ed8fe92999efec6645880
calebeLAR <ca6ele@gmail.com>
2024-05-24 17:02:39 -0300

    trybe.front-end.secao06.dia05.Unit01.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia05_testes_em_react_redux/segment01/Unit01.mjs
@@ -0,0 +1,10 @@
+export default class Unit01 {
+  static title = 'Introdução - Testando React e Redux';
+  static topics = [
+    'Introdução - Testando React e Redux',
+    'O que vamos aprender?',
+    'Você será capaz de:',
+    'Por que isso é importante?',
+  ];
+  static videos = ['[FE] 6.5 - Testando o Redux Introdução'];
+}
