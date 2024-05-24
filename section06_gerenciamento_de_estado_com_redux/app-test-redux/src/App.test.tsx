d3ca8de12dd4ebc0b9d3022ee7eb335fc23ae81d
calebeLAR <ca6ele@gmail.com>
2024-05-24 16:46:47 -0300

    upadate

--- section06_gerenciamento_de_estado_com_redux/app-test-redux/src/App.test.tsx
+++ section06_gerenciamento_de_estado_com_redux/app-test-redux/src/App.test.tsx
@@ -24,3 +24,27 @@ test('O número renderizado na página deve ser o mesmo valor do estado global',
   expect(screen.queryByText('0')).not.toBeInTheDocument();
   expect(screen.getByText('10')).toBeInTheDocument();
 });
+
+test('Incrementa o valor da store ao clicar no botão', async () => {
+  const { store, user } = renderWithRedux(<App />);
+  expect(screen.getByText('0')).toBeInTheDocument();
+  expect(store.getState().counterReducer.count).toBe(0);
+
+  const button = screen.getByText('Incrementa 1');
+  await user.click(button);
+
+  expect(screen.getByText('1')).toBeInTheDocument(); // testa se o valor 1 é renderizado
+  expect(store.getState().counterReducer.count).toBe(1); // testa se o count do Redux é 1
+});
+
+test('Incrementa o valor da store ao clicar no botão', async () => {
+  const { store, user } = renderWithRedux(<App />);
+  expect(screen.getByText('0')).toBeInTheDocument();
+  expect(store.getState().counterReducer.count).toBe(0);
+
+  const button = screen.getByText('Incrementa 1');
+  await user.click(button);
+
+  expect(screen.getByText('1')).toBeInTheDocument(); // testa se o valor 1 é renderizado
+  expect(store.getState().counterReducer.count).toBe(1); // testa se o count do Redux é 1
+});
