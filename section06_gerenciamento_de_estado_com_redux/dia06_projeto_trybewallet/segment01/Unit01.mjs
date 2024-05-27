f39b742144c180b197f6b36f3a5f532116800085
calebeLAR <ca6ele@gmail.com>
2024-05-27 15:21:54 -0300

    trybe.front-end.secao06.dia06.Unit01.mjs

---
+++ section06_gerenciamento_de_estado_com_redux/dia06_projeto_trybewallet/segment01/Unit01.mjs
@@ -0,0 +1,14 @@
+export default class Unit01 {
+  static title = 'Projeto - TrybeWallet';
+  static topics = [
+    'Projeto - TrybeWallet',
+    'O que vamos fazer?',
+    'Por que isso é importante?',
+    'Especificação',
+    'O que vamos avaliar?',
+    'O que devo desenvolver?',
+    'Entregáveis',
+    'Fique atento e siga as instruções no README.md do repositório! 🥺',
+  ];
+  static videos = [];
+}
