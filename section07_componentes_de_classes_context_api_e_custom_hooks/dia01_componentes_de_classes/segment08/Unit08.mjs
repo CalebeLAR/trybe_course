a9887964f6602814d83fdb4f253955915c4147f8
calebeLAR <ca6ele@gmail.com>
2024-05-29 16:05:59 -0300

    trybe.front-end.secao07.dia01.Unit08.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia01_componentes_de_classes/segment08/Unit08.mjs
@@ -0,0 +1,5 @@
+export default class Unit08 {
+  static title = 'Exercícios - Teste seus conhecimentos';
+  static topics = ['Exercícios - Teste seus conhecimentos', 'Nobel Prizes'];
+  static videos = [];
+}
