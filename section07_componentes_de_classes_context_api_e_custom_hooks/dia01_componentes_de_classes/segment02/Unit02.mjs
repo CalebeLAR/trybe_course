e7a06e8c112f7e23c8c3115ffc0ec1506fa27d07
calebeLAR <ca6ele@gmail.com>
2024-05-29 16:05:59 -0300

    trybe.front-end.secao07.dia01.Unit02.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia01_componentes_de_classes/segment02/Unit02.mjs
@@ -0,0 +1,9 @@
+export default class Unit02 {
+  static title = 'Criando um componente de classe';
+  static topics = [
+    'Criando um componente de classe',
+    'O que é classe?',
+    'Criando o componente com o uso da classe React.Component',
+  ];
+  static videos = ['FE 7.1 - Criando um componente de classe'];
+}
