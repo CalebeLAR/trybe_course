0c30dfb39130f6e03e844233da0898160d308e93
calebeLAR <ca6ele@gmail.com>
2024-05-30 15:12:08 -0300

    trybe.front-end.secao07.dia02.Unit05.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia02_context_api/segment05/Unit05.mjs
@@ -0,0 +1,5 @@
+export default class Unit05 {
+  static title = 'Finalizando a aplicação';
+  static topics = ['Finalizando a aplicação', 'Estilizando a aplicação'];
+  static videos = ['Renato Freire * FE 7.2 - Finalizando a aplicação'];
+}
