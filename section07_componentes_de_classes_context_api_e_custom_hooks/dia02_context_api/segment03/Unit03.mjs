7c95ec8d35ce5f94fc61e42d3a4c16bb818eaa2c
calebeLAR <ca6ele@gmail.com>
2024-05-30 15:12:08 -0300

    trybe.front-end.secao07.dia02.Unit03.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia02_context_api/segment03/Unit03.mjs
@@ -0,0 +1,7 @@
+export default class Unit03 {
+  static title = 'Lendo os dados do Context';
+  static topics = ['Lendo os dados do Context'];
+  static videos = [
+    'Renato Freire * FE 7.2 - Lendo informações do Context com o hook useContext',
+  ];
+}
