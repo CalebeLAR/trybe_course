4f4963607ec0b232f0add961bfae466b50212033
calebeLAR <ca6ele@gmail.com>
2024-05-30 15:12:08 -0300

    trybe.front-end.secao07.dia02.Unit02.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia02_context_api/segment02/Unit02.mjs
@@ -0,0 +1,10 @@
+export default class Unit02 {
+  static title = 'Criando e disponibilizando o Context';
+  static topics = [
+    'Criando e disponibilizando o Context',
+    'Criando o contexto',
+    'Componentes Provider e Consumer',
+    'Disponibilizando o contexto',
+  ];
+  static videos = ['Renato Freire * FE 7.2 - Criando um  Context com createContext'];
+}
