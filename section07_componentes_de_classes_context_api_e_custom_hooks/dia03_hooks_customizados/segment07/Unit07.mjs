71d56cdcd216644cb0ea2b0d109074c1c417d98d
calebeLAR <ca6ele@gmail.com>
2024-05-31 15:03:43 -0300

    trybe.front-end.secao07.dia03.Unit07.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia03_hooks_customizados/segment07/Unit07.mjs
@@ -0,0 +1,9 @@
+export default class Unit07 {
+  static title = 'Fixando os aprendizados da seção';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '60%',
+    '6 de 10 correta(s)',
+  ];
+  static videos = [];
+}
