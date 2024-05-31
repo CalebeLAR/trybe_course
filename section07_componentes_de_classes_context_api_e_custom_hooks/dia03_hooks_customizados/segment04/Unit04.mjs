e79765c149d4b1827c8514924d0aecf3226a3441
calebeLAR <ca6ele@gmail.com>
2024-05-31 15:03:43 -0300

    trybe.front-end.secao07.dia03.Unit04.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia03_hooks_customizados/segment04/Unit04.mjs
@@ -0,0 +1,8 @@
+export default class Unit04 {
+  static title = 'Exercício de fixação';
+  static topics = [
+    'Exercício de fixação',
+    'Exercício - Criando o Hook Customizado useToggle',
+  ];
+  static videos = [];
+}
