90d532bc04678c088606e83738f27cc10522b3ea
calebeLAR <ca6ele@gmail.com>
2024-05-31 15:03:43 -0300

    trybe.front-end.secao07.dia03.Unit01.mjs

---
+++ section07_componentes_de_classes_context_api_e_custom_hooks/dia03_hooks_customizados/segment01/Unit01.mjs
@@ -0,0 +1,10 @@
+export default class Unit01 {
+  static title = 'Introdução';
+  static topics = [
+    'Introdução',
+    'O que vamos aprender?',
+    'Você será capaz de',
+    'Por que isso é importante?',
+  ];
+  static videos = [];
+}
