9da17e3d11f8ccf0ac28d001dad4801c37859ab2
calebeLAR <ca6ele@gmail.com>
2024-06-24 12:17:42 -0300

    trybe.front-end.secao01.dia01.Unit10.mjs

---
+++ section01_docker_utilizando_containers/dia01_utilizando_containers_docker/segment10/Unit10.mjs
@@ -0,0 +1,20 @@
+export default class Unit10 {
+  static title = 'Exercícios - agora, a prática';
+  static topics = [
+    'Exercícios - agora, a prática',
+    '🚀 Exercício 1',
+    '🚀 Exercício 2',
+    '🚀 Exercício 3',
+    '🚀 Exercício 4',
+    '🚀 Exercício 5',
+    '🚀 Exercício 6',
+    '🚀 Exercício 8',
+    '🚀 Exercício 9',
+    '🚀 Exercício 10',
+    '🚀 Exercício 11',
+    '🚀 Exercício 12',
+    'Exercício 13',
+    'Exercício 14',
+  ];
+  static videos = [];
+}
