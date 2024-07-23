c5f0eed49e0fbeb4426ca834610b40e403125882
calebeLAR <ca6ele@gmail.com>
2024-07-23 12:52:18 -0300

    trybe.back-end.secao01.dia02.Unit01.mjs

---
+++ section01_docker_utilizando_containers/dia02_manipulando_imagens_no_docker/segment01/Unit01.mjs
@@ -0,0 +1,12 @@
+export default class Unit01 {
+  static title = 'Introdução';
+  static topics = [
+    'Introdução - Manipulação e Criação de Imagens no Docker',
+    'O que vamos aprender?',
+    'Você será capaz de',
+    'Por que isso é importante?',
+  ];
+  static videos = [
+    { prof: 'Cristiano Cunha', title: 'Doker Imagens Introdução' },
+  ];
+}
