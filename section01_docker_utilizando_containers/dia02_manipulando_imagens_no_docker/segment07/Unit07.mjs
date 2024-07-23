8f264fe40b129ad858947eef66d706c27dd73ed2
calebeLAR <ca6ele@gmail.com>
2024-07-23 12:52:18 -0300

    trybe.back-end.secao01.dia02.Unit07.mjs

---
+++ section01_docker_utilizando_containers/dia02_manipulando_imagens_no_docker/segment07/Unit07.mjs
@@ -0,0 +1,10 @@
+export default class Unit07 {
+  static title = 'Recapitulando';
+  static topics = ['Recapitulando'];
+  static videos = [
+    {
+      prof: 'Cristiano Cunha',
+      title: 'Docker Images - Praticando Com o Docker File',
+    },
+  ];
+}
