6c1fbc7f9e8b086a60443eb4ce30d58f07e7e95d
calebeLAR <ca6ele@gmail.com>
2024-07-23 12:52:18 -0300

    trybe.back-end.secao01.dia02.Unit04.mjs

---
+++ section01_docker_utilizando_containers/dia02_manipulando_imagens_no_docker/segment04/Unit04.mjs
@@ -0,0 +1,11 @@
+export default class Unit04 {
+  static title = 'Criando nosso primeiro servidor Web';
+  static topics = [
+    'Criando nosso primeiro servidor Web',
+    'Limpando tudo antes de começar!',
+    'Construção da imagem',
+    'Executando um novo container com nossa imagem',
+    'Recapitulando ✏️',
+  ];
+  static videos = [{ prof: 'Cristiano Cunha', title: 'Docker Images Mapeamento de Portas' }];
+}
