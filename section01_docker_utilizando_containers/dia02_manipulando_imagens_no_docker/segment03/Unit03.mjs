8e1517df435ad804976b21c56bae62d4279bd51c
calebeLAR <ca6ele@gmail.com>
2024-07-23 12:52:18 -0300

    trybe.back-end.secao01.dia02.Unit03.mjs

---
+++ section01_docker_utilizando_containers/dia02_manipulando_imagens_no_docker/segment03/Unit03.mjs
@@ -0,0 +1,12 @@
+export default class Unit03 {
+  static title = 'Criando nossa primeira imagem Docker';
+  static topics = [
+    'Criando nossa primeira imagem Docker',
+    'Limpando tudo antes de começar!',
+    'Um passo de cada vez!',
+    'Passo 1 - Criando o arquivo Dockerfile',
+    'Passo 2 - Construindo a imagem Docker',
+    'Executando um novo container com nossa imagem',
+  ];
+  static videos = [{ prof: '', title: '' }];
+}
