fb3bca6038840e5c2a301da75321953b720191fc
calebeLAR <ca6ele@gmail.com>
2024-07-23 12:52:18 -0300

    trybe.back-end.secao01.dia02.Unit11.mjs

---
+++ section01_docker_utilizando_containers/dia02_manipulando_imagens_no_docker/segment11/Unit11.mjs
@@ -0,0 +1,16 @@
+export default class Unit11 {
+  static title = 'Recursos adicionais (opcional)';
+  static topics = ['Recursos adicionais (opcional)'];
+  static videos = [{ prof: '', title: '' }];
+}
+
+// Docker - Limpando contêineres, imagens e volumes
+// http://www.macoratti.net/19/02/dock_limp1.htm
+// Documentação oficial do Docker - About storage drivers
+// https://docs.docker.com/storage/storagedriver/
+// Docker Layers Explained
+// https://dzone.com/articles/docker-layers-explained
+// Documentação oficial do Docker - Docker images
+// https://docs.docker.com/engine/reference/commandline/images/
+// Documentação oficial do Docker - Dockerfile
+// https://docs.docker.com/engine/reference/builder/
