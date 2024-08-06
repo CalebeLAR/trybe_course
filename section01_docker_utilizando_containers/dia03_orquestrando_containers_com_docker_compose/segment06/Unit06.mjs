67e7f8b7de699e73e91ff16d324a0ce08f382288
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit06.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment06/Unit06.mjs
@@ -0,0 +1,8 @@
+export default class Unit06 {
+  static title = 'Serviço vs. Container';
+  static topics = [
+    'Serviço vs. Container',
+    'Outros orquestradores de containers',
+  ];
+  static videos = [];
+}
