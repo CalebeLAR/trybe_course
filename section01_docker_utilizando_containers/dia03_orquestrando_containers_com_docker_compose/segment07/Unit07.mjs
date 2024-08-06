4f4e2c498f97329132619410849dc57b3405e718
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit07.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment07/Unit07.mjs
@@ -0,0 +1,8 @@
+export default class Unit07 {
+  static title = 'Bônus: Tópicos extras sobre Docker';
+  static topics = ['Bônus: Tópicos extras sobre Docker'];
+  static videos = [];
+}
+
+// Tópicos extras sobre Docker
+// https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/f04cdb21-382e-4588-8950-3b1a29afd2dd/section/4a9c025a-d8d9-4a87-92ac-0903a07407b6/lesson/50140c0d-e359-4f95-9847-96699ac52449
