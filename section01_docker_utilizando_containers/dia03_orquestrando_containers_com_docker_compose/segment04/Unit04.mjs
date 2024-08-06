6d5707af4392531b3e3825849851a279a8b9edf5
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit04.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment04/Unit04.mjs
@@ -0,0 +1,13 @@
+export default class Unit04 {
+  static title = 'Gerenciando nossos Serviços';
+  static topics = [
+    'Gerenciando nossos Serviços',
+    'Subindo todos os serviços',
+    'Verificando o status dos serviços',
+    'Reconstruindo a Imagem Docker',
+    'Descendo todos os serviços',
+    'Subindo serviços específicos',
+    'Visualizando os logs dos serviços',
+  ];
+  static videos = [];
+}
