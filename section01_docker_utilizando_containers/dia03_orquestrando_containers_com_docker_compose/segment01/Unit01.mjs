23a6fbf5d173dbaa7b638141fb3dcce3e93e0631
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit01.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment01/Unit01.mjs
@@ -0,0 +1,12 @@
+export default class Unit01 {
+  static title = 'Introdução';
+  static topics = [
+    'Introdução',
+    'O que vamos aprender?',
+    'Você será capaz de',
+    'Por que isso é importante?',
+  ];
+  static videos = [
+    { prof: 'Cristiano Cunha', title: 'Docker Compose Introdução' },
+  ];
+}
