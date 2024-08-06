e9c40b3adc8e3d238a3e50eae2bce589660c33f3
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit03.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment03/Unit03.mjs
@@ -0,0 +1,14 @@
+export default class Unit03 {
+  static title = 'Criando nosso primeiro arquivo Compose';
+  static topics = [
+    'Criando nosso primeiro arquivo Compose',
+    'Estruturando nosso projeto',
+    'Versão do Arquivo Compose',
+    'Criando Serviços',
+    'Mapeamento de Portas',
+    'E se o container apresentar um problema?',
+    'Usando Variáveis de Ambiente',
+    'Dependência entre Serviços',
+  ];
+  static videos = [];
+}
