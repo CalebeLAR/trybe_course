4926c6cd73a12a540c5d61d3ce0c508226f0ca31
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:22 -0300

    trybe.back-end.secao01.dia03.Unit05.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment05/Unit05.mjs
@@ -0,0 +1,15 @@
+export default class Unit05 {
+  static title = 'Arquivos Compose mais robustos';
+  static topics = [
+    'Arquivos Compose mais robustos',
+    'Preparando o nosso projeto',
+    'Criando Volumes',
+    'Criando redes virtuais',
+  ];
+  static videos = [
+    {
+      prof: 'Cristiano Cunha',
+      title: 'Docker Compose - Orquestrando Containers com o Compose',
+    },
+  ];
+}
