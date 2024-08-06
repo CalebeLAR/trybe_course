c3944a7d95c2a603793ef2b02a531ac22ea97a51
calebeLAR <ca6ele@gmail.com>
2024-08-06 12:27:23 -0300

    trybe.back-end.secao01.dia03.Unit12.mjs

---
+++ section01_docker_utilizando_containers/dia03_orquestrando_containers_com_docker_compose/segment12/Unit12.mjs
@@ -0,0 +1,15 @@
+export default class Unit12 {
+  static title = 'Recursos adicionais (opcional)';
+  static topics = ['Recursos adicionais (opcional)'];
+  static videos = [];
+}
+
+
+// Documentação oficial do Docker - Use volumes
+// https://docs.docker.com/storage/volumes/
+// Configurando opções de rede dos containers
+// https://www.youtube.com/watch?v=pKJgQmXXryg
+// Manage sensitive data with Docker secrets
+// https://docs.docker.com/engine/swarm/secrets/
+// Overview of Docker Compose
+// https://docs.docker.com/compose/
