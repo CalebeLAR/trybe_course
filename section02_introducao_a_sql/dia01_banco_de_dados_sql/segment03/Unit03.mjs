76cc7d596c08843cf501ce79cd28bfe5fd95a212
calebeLAR <ca6ele@gmail.com>
2024-08-20 18:18:09 -0300

    trybe.back-end.secao02.dia01.Unit03.mjs

---
+++ section02_introducao_a_sql/dia01_banco_de_dados_sql/segment03/Unit03.mjs
@@ -0,0 +1,5 @@
+export default class Unit03 {
+  static title = 'O que é e quais são os tipos de bancos de dados';
+  static topics = ['O que é e quais são os tipos de bancos de dados'];
+  static videos = [{ prof: 'Gustavo Caetano', title: 'Banco de Dados' }];
+}
