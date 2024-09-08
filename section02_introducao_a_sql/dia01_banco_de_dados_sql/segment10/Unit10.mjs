dd1feb50412ecbdce7e97d2999e44007a3cfd2bb
calebeLAR <ca6ele@gmail.com>
2024-09-08 14:16:11 -0300

    trybe.back-end.secao02.dia01.Unit10.mjs

---
+++ section02_introducao_a_sql/dia01_banco_de_dados_sql/segment10/Unit10.mjs
@@ -0,0 +1,8 @@
+export default class Unit10 {
+  static title =
+    'Utilizando as principais features do workbench (Crash course do workbench)';
+  static topics = [
+    'Utilizando as principais features do workbench (Crash course do workbench)',
+  ];
+  static videos = [{ prof: 'Gustavo Caetano', title: 'Workbanch' }];
+}
