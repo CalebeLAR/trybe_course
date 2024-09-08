eaf0d319fa024bd3380fa267cdb207c8c89097cd
calebeLAR <ca6ele@gmail.com>
2024-09-08 14:16:11 -0300

    trybe.back-end.secao02.dia01.Unit09.mjs

---
+++ section02_introducao_a_sql/dia01_banco_de_dados_sql/segment09/Unit09.mjs
@@ -0,0 +1,5 @@
+export default class Unit09 {
+  static title = 'Restaurando o banco de dados de prática sakila';
+  static topics = ['Restaurando o banco de dados de prática sakila'];
+  static videos = [{ prof: '', title: '' }];
+}
