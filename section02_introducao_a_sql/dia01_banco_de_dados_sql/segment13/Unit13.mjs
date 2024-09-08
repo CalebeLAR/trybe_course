bd3fae710019cfc84b51283c96d04b1f2d9ffbe9
calebeLAR <ca6ele@gmail.com>
2024-09-08 14:16:11 -0300

    trybe.back-end.secao02.dia01.Unit13.mjs

---
+++ section02_introducao_a_sql/dia01_banco_de_dados_sql/segment13/Unit13.mjs
@@ -0,0 +1,10 @@
+export default class Unit13 {
+  static title = 'Como os dados são ligados?';
+  static topics = [
+    'Como os dados são ligados?',
+    'Um para Um',
+    'Um para Muitos ou Muitos para Um',
+    'Muitos para Muitos',
+  ];
+  static videos = [{ prof: 'Gustavo Caetano', title: 'Tipos de Relacionamentos' }];
+}
