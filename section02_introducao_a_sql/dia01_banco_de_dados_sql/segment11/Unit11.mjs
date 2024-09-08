fe232353f652a4a6a3a6f0ac550106f01dbb69b9
calebeLAR <ca6ele@gmail.com>
2024-09-08 14:16:11 -0300

    trybe.back-end.secao02.dia01.Unit11.mjs

---
+++ section02_introducao_a_sql/dia01_banco_de_dados_sql/segment11/Unit11.mjs
@@ -0,0 +1,9 @@
+export default class Unit11 {
+  static title =
+    'Constraints (restrições), chaves primárias e chaves estrangeiras';
+  static topics = [
+    'Constraints (restrições), chaves primárias e chaves estrangeiras',
+    'Exercícios de fixação',
+  ];
+  static videos = [{ prof: 'Gustavo Caetano', title: 'Constraint' }];
+}
