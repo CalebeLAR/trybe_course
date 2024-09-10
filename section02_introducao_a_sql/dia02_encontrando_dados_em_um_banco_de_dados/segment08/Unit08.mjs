205e3cb26bcc11756aba8dbf8f96f4664a8261c9
calebeLAR <ca6ele@gmail.com>
2024-09-10 16:04:24 -0300

    trybe.back-end.secao02.dia02.Unit08.mjs

---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment08/Unit08.mjs
@@ -0,0 +1,5 @@
+export default class Unit08 {
+  static title = 'LIMIT OFFSET - Pulando linhas desnecessárias';
+  static topics = ['LIMIT OFFSET - Pulando linhas desnecessárias'];
+  static videos = [{ prof: '', title: '' }];
+}
