215b1e8894dc533c93189272d4a2f52f733f6da4
calebeLAR <ca6ele@gmail.com>
2024-09-10 16:04:24 -0300

    trybe.back-end.secao02.dia02.Unit04.mjs

---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment04/Unit04.mjs
@@ -0,0 +1,5 @@
+export default class Unit04 {
+  static title = 'CONCAT - juntando duas ou mais colunas';
+  static topics = ['CONCAT - juntando duas ou mais colunas'];
+  static videos = [{ prof: '', title: '' }];
+}
---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment04/pratica.sql
@@ -0,0 +1,4 @@
+--     Na tabela sakila.film, monte uma query que exiba o título e o ano de lançamento dos filmes em uma coluna e dê a ela o nome Lançamento do Filme.
+SELECT concat(title,": ", release_year) as 'Lançamento do Filme' FROM sakila.film;
+--     Na tabela sakila.address, monte uma query que exiba a rua e o distrito de cada registro em uma coluna apenas, e dê a essa coluna o nome Endereço.
+SELECT concat(address,"		", district) as 'Endereço' FROM sakila.address;
