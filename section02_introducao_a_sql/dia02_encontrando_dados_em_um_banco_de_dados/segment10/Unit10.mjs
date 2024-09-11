dec0b82ab8d5ea923a4c6624ea6aa93f0500de42
calebeLAR <ca6ele@gmail.com>
2024-09-11 16:13:28 -0300

    trybe.back-end.secao02.dia02.Unit10.mjs

---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment10/Unit10.mjs
@@ -0,0 +1,8 @@
+export default class Unit10 {
+  static title = 'Juntando tudo que vimos hoje';
+  static topics = [
+    'Juntando tudo que vimos hoje',
+    'Bônus: Exercícios para fixar',
+  ];
+  static videos = [{ prof: '', title: '' }];
+}
---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment10/bonus.sql
@@ -0,0 +1,48 @@
+-- Bônus: Exercícios para fixar
+
+-- Agora é a sua vez! Vamos juntar tudo isso na prática usando a tabela sakila.film:
+-- Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
+SELECT * FROM sakila.film;
+
+-- Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
+SELECT title, release_year, rating FROM sakila.film;
+
+-- Quantos filmes temos cadastrados?
+SELECT count(*) FROM sakila.film;
+
+
+-- Para os exercícios a seguir, vamos usar a tabela sakila.actor:
+-- Escreva uma query que exiba apenas os sobrenomes únicos cadastrados.
+SELECT DISTINCT last_name FROM sakila.actor;
+
+-- Quantos sobrenomes únicos temos na tabela?
+SELECT count(DISTINCT last_name) FROM sakila.actor;
+
+-- Ordene os valores na tabela em ordem crescente de sobrenomes e em ordem decrescente de nome.
+SELECT first_name, last_name FROM sakila.actor
+	ORDER BY first_name DESC, last_name ASC;
+
+
+-- Usando a tabela language:
+-- Crie uma pesquisa que mostre os 5 idiomas cadastrados, mas não mostre o idioma english.
+SELECT * FROM sakila.`language` WHERE `name` != "English";
+SELECT * FROM sakila.`language` LIMIT 6 OFFSET 1;
+
+-- Usando a tabela film:
+-- Selecione todos os dados da tabela. Pronto, fez isso?
+SELECT * FROM sakila.film;
+
+-- 1.1 Agora vamos tentar fazer o seguinte: Crie uma query para encontrar os 20 primeiros filmes,
+SELECT * FROM sakila.film LIMIT 20; 
+
+-- incluindo o título, o ano de lançamento, a duração, a classificação indicativa e o custo de substituição.
+SELECT title, release_year, length, rating, replacement_cost FROM sakila.film LIMIT 20;
+
+-- Ordene os resultados pelos filmes com a maior duração e depois pelo menor custo de substituição.
+SELECT 
+    title, release_year, length, rating, replacement_cost
+FROM
+    sakila.film
+ORDER BY length DESC , replacement_cost ASC
+LIMIT 20;
+
