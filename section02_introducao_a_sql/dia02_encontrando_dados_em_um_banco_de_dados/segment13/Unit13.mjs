d1c9b3c16561f1db92ee3621a8a86064baa7c65e
calebeLAR <ca6ele@gmail.com>
2024-09-11 16:13:29 -0300

    trybe.back-end.secao02.dia02.Unit13.mjs

---
+++ section02_introducao_a_sql/dia02_encontrando_dados_em_um_banco_de_dados/segment13/Unit13.mjs
@@ -0,0 +1,5 @@
+export default class Unit13 {
+  static title = 'Exercícios - antes de começar';
+  static topics = ['Exercícios - antes de começar', 'Versionando seu código'];
+  static videos = [{ prof: '', title: '' }];
+}
