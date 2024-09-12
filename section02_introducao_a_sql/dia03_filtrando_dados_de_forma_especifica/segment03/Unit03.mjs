87afb5d1d6da869b319e44e16ff49fd0f6061e0b
calebeLAR <ca6ele@gmail.com>
2024-09-12 17:30:59 -0300

    trybe.back-end.secao02.dia03.Unit03.mjs

---
+++ section02_introducao_a_sql/dia03_filtrando_dados_de_forma_especifica/segment03/Unit03.mjs
@@ -0,0 +1,5 @@
+export default class Unit03 {
+  static title = 'Operadores booleanos e relacionais';
+  static topics = ['Operadores booleanos e relacionais', 'Para Fixar'];
+  static videos = [{ prof: '', title: '' }];
+}
