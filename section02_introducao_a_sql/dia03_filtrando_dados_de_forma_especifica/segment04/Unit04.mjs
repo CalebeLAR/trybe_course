83550f639f7b0f86ebfc34453d84b23d63de03e9
calebeLAR <ca6ele@gmail.com>
2024-09-12 17:30:59 -0300

    trybe.back-end.secao02.dia03.Unit04.mjs

---
+++ section02_introducao_a_sql/dia03_filtrando_dados_de_forma_especifica/segment04/Unit04.mjs
@@ -0,0 +1,5 @@
+export default class Unit04 {
+  static title = '`LIKE` - criando pesquisas mais dinâmicas';
+  static topics = ['LIKE - criando pesquisas mais dinâmicas', 'Para Fixar'];
+  static videos = [{ prof: '', title: '' }];
+}
