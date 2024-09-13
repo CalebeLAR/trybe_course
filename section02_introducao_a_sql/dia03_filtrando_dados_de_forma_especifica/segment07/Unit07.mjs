fb6a04160f3b6c3217681c631c0ada46a7a2b869
calebeLAR <ca6ele@gmail.com>
2024-09-13 15:18:53 -0300

    trybe.back-end.secao02.dia03.Unit07.mjs

---
+++ section02_introducao_a_sql/dia03_filtrando_dados_de_forma_especifica/segment07/Unit07.mjs
@@ -0,0 +1,9 @@
+export default class Unit07 {
+  static title = 'Fixando os aprendizados';
+  static topics = [
+    'Fixando os aprendizados - Resultado',
+    '20%',
+    '2 de 10 correta(s)',
+  ];
+  static videos = [{ prof: '', title: '' }];
+}
