4d95972ba41da184332a2c6f4cec296955b44f5d
calebeLAR <ca6ele@gmail.com>
2024-09-17 18:01:00 -0300

    trybe.back-end.secao02.dia04.Unit04.mjs

---
+++ section02_introducao_a_sql/dia04_manipulando_tabelas/segment04/Unit04.mjs
@@ -0,0 +1,9 @@
+export default class Unit04 {
+  static title = '`UPDATE` - alterando dados';
+  static topics = [
+    'UPDATE - alterando dados',
+    'Alterando mais de uma coluna ao mesmo tempo',
+    'UPDATE em massa',
+  ];
+  static videos = [{ prof: '', title: '' }];
+}
