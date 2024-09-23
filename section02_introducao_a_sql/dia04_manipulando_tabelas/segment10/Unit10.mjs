75228eb8ffb436493e4e84ebd536d424dc38e9b6
calebeLAR <ca6ele@gmail.com>
2024-09-23 17:59:14 -0300

    trybe.back-end.secao02.dia04.Unit10.mjs

---
+++ section02_introducao_a_sql/dia04_manipulando_tabelas/segment10/Unit10.mjs
@@ -0,0 +1,5 @@
+export default class Unit10 {
+  static title = 'Exercícios - agora, a prática';
+  static topics = ['Exercícios - agora, a prática'];
+  static videos = [{ prof: '', title: '' }];
+}
