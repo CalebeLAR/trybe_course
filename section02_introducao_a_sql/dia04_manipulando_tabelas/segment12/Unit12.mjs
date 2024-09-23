53cd557ffb900ef7476a99f3fe93ca97623188b2
calebeLAR <ca6ele@gmail.com>
2024-09-23 17:59:14 -0300

    trybe.back-end.secao02.dia04.Unit12.mjs

---
+++ section02_introducao_a_sql/dia04_manipulando_tabelas/segment12/Unit12.mjs
@@ -0,0 +1,27 @@
+export default class Unit12 {
+  static title = 'Recursos adicionais (opcional)';
+  static topics = ['Recursos adicionais (opcional)'];
+  static videos = [{ prof: '', title: '' }];
+}
+
+
+//  Tutorial sobre INSERT do Guru99
+//  https://www.guru99.com/insert-into.html
+
+//  Tutorial sobre INSERT do MySQL Tutorial
+//  https://www.mysqltutorial.org/mysql-basics/mysql-insert/  
+
+//  Tutorial sobre UPDATE do MySQL Tutorial
+//  https://www.mysqltutorial.org/mysql-basics/mysql-update/
+
+//  Tutorial sobre DELETE e UPDATE do Guru99
+//  https://www.guru99.com/delete-and-update.html
+
+//  Tutorial sobre DELETE do MySQL Tutorial
+//  https://www.mysqltutorial.org/mysql-basics/mysql-delete/
+
+//  Tutorial sobre DELETE do Tech On The Net
+//  https://www.techonthenet.com/mysql/delete.php
+
+//  Documentação sobre restrições de chaves estrangeiras no MySQL
+//  https://dev.mysql.com/doc/refman/8.0/en/create-table-foreign-keys.html
